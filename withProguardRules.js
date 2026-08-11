4286e4d31fafc54707b6c58f167e63d8217fb6c7
Webnix <sanibmalik850@gmail.com>
2026-08-11 18:37:26 +0530

    Update withProguardRules.js

--- withProguardRules.js
+++ withProguardRules.js
@@ -1,10 +1,9 @@
 const { withAppBuildGradle } = require('@expo/config-plugins');
-
 module.exports = function withProguardRules(config) {
   return withAppBuildGradle(config, (config) => {
     if (!config.modResults.contents.includes('proguard-rules-webnix.pro')) {
       config.modResults.contents = config.modResults.contents.replace(
-        /proguardFiles/,
+        /proguardFiles/g,
         `proguardFile "${__dirname}/proguard-rules-webnix.pro"\n            proguardFiles`
       );
     }
